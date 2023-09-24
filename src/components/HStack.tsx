80251980ed6415c75938998865b79afea557e8c3
Carlos Garate <carlos.m.garate@gmail.com>
2023-09-24 15:14:27 -0400

    add common components

---
+++ src/components/HStack.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Box } from "./Box";
+
+export const HStack = styled(Box)`
+  display: flex;
+  flex-direction: row;
+`;
---
+++ src/components/StyledH1.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledH1 = styled.h1`
+  font-family: Helvetica;
+  font-size: 1.5rem;
+`;
