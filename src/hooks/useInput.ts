0954e32a05fd8d8a5484c18f11afa1900196dbfc
Carlos Garate <carlos.m.garate@gmail.com>
2023-09-24 14:30:15 -0400

    add resetHandler

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -4,8 +4,9 @@ const useInput = (inputValue: HTMLInputElement["value"]) => {
   const [value, setValue] = useState(inputValue);
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) =>
     setValue(event.target.value);
+  const resetHandler = () => setValue("");
 
-  return { onChange: onChangeHandler, value };
+  return { onChange: onChangeHandler, value, reset: resetHandler };
 };
 
 export default useInput;
