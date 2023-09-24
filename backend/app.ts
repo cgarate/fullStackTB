f6a01a5a07bf77d28d406ad4e5f1baf4fd76dfdf
Carlos Garate <carlos.m.garate@gmail.com>
2023-09-23 23:53:35 -0400

    add cors to express

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import logger from "morgan";
 const medianRouter = require("./routes/median");
+const cors = require('cors')
 
 const port = 3000;
 
 const app = express();
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", medianRouter);
