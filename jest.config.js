637be826ddf47ec951defb04177d73a5e2558f63
Carlos Garate <carlos.m.garate@gmail.com>
2023-09-25 01:46:00 -0400

    remove setup file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ["./backend"],
